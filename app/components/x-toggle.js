1e50bbe8a7a2bbf440ab3f3474dab4a7538365d7
Ken Snyder <ken@ken.net>
2014-12-26 00:38:41 +0000

    Added VALUE binding

--- app/components/x-toggle.js
+++ app/components/x-toggle.js
@@ -11,9 +11,16 @@ export default Ember.Component.extend({
   classNames: ['x-toggle-container'],
   theme: config.defaultTheme || 'default',
   off: config.defaultOff || 'Off',
+  offLabel: computed('off', function() {
+  	return this.get('off').indexOf(':') > -1 ? this.get('off').substr(0,this.get('off').indexOf(':')) : this.get('off');
+  }),
   on: config.defaultOn || 'On',
+  onLabel: computed('on', function() {
+  	return this.get('on').indexOf(':') > -1 ? this.get('on').substr(0,this.get('on').indexOf(':')) : this.get('on');
+  }),
   showLabels: config.defaultShowLabels || false,
   size: config.defaultSize || 'medium',
+  value: false,
   toggled: false,
 
   themeClass: computed('theme', function () {
@@ -28,7 +35,27 @@ export default Ember.Component.extend({
 
   wasToggled: on('init', observer('toggled', function () {
     var toggled = this.get('toggled');
-    
+	var offState = this.get('off').substr(this.get('off').indexOf(':')+1) || false;
+	var onState = this.get('on').substr(this.get('on').indexOf(':')+1) || true;	
     this.sendAction('toggle', toggled);
+	if(toggled === false) {
+		this.set('value',offState);
+	} else {
+		this.set('value',onState);
+	}
+  })),
+  valueObserver: on('init', observer('value', function() {
+	  Ember.run.debounce(this, function() {
+	  	  var value = this.get('value');
+	  	  var offState = this.get('off').substr(this.get('off').indexOf(':')+1) || false;
+	  	  var onState = this.get('on').substr(this.get('on').indexOf(':')+1) || true;
+	  	  if(value === onState) {
+			  this.set('toggled', true);
+	  	  } else {
+	  		  this.set('toggled', false);
+			  this.set('value',offState);
+	  	  }
+	  }, 1000);
   }))
+  
 });
