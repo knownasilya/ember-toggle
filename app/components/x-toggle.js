4633a4dccdad49f281aa88874e99106249eae7df
Ilya Radchenko <ilya@burstcreations.com>
2015-01-19 12:44:26 -0500

    Cleanup syntax

--- app/components/x-toggle.js
+++ app/components/x-toggle.js
@@ -11,17 +11,23 @@ export default Ember.Component.extend({
   classNames: ['x-toggle-container'],
   theme: config.defaultTheme || 'default',
   off: config.defaultOff || 'Off',
-  offLabel: computed('off', function() {
-  	return this.get('off').indexOf(':') > -1 ? this.get('off').substr(0,this.get('off').indexOf(':')) : this.get('off');
-  }),
   on: config.defaultOn || 'On',
-  onLabel: computed('on', function() {
-  	return this.get('on').indexOf(':') > -1 ? this.get('on').substr(0,this.get('on').indexOf(':')) : this.get('on');
-  }),
   showLabels: config.defaultShowLabels || false,
   size: config.defaultSize || 'medium',
   value: false,
   toggled: false,
+
+  onLabel: computed('on', function () {
+  	return this.get('on').indexOf(':') > -1
+      ? this.get('on').substr(0,this.get('on').indexOf(':'))
+      : this.get('on');
+  }),
+
+  offLabel: computed('off', function () {
+  	return this.get('off').indexOf(':') > -1
+      ? this.get('off').substr(0,this.get('off').indexOf(':'))
+      : this.get('off');
+  }),
 
   themeClass: computed('theme', function () {
     var theme = this.get('theme') || 'default';
@@ -35,27 +41,30 @@ export default Ember.Component.extend({
 
   wasToggled: on('init', observer('toggled', function () {
     var toggled = this.get('toggled');
-	var offState = this.get('off').substr(this.get('off').indexOf(':')+1) || false;
-	var onState = this.get('on').substr(this.get('on').indexOf(':')+1) || true;	
+    var offState = this.get('off').substr(this.get('off').indexOf(':') + 1) || false;
+    var onState = this.get('on').substr(this.get('on').indexOf(':') + 1) || true;
+
     this.sendAction('toggle', toggled);
-	if(toggled === false) {
-		this.set('value',offState);
-	} else {
-		this.set('value',onState);
-	}
+
+    if (toggled === false) {
+      this.set('value', offState);
+    } else {
+      this.set('value', onState);
+    }
   })),
+
   valueObserver: on('init', observer('value', function() {
-	  Ember.run.debounce(this, function() {
+	  Ember.run.debounce(this, function () {
 	  	  var value = this.get('value');
-	  	  var offState = this.get('off').substr(this.get('off').indexOf(':')+1) || false;
-	  	  var onState = this.get('on').substr(this.get('on').indexOf(':')+1) || true;
-	  	  if(value === onState) {
-			  this.set('toggled', true);
+	  	  var offState = this.get('off').substr(this.get('off').indexOf(':') + 1) || false;
+	  	  var onState = this.get('on').substr(this.get('on').indexOf(':') + 1) || true;
+
+	  	  if (value === onState) {
+          this.set('toggled', true);
 	  	  } else {
 	  		  this.set('toggled', false);
-			  this.set('value',offState);
+          this.set('value', offState);
 	  	  }
 	  }, 500);
   }))
-  
 });
