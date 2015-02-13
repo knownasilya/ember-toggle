12a7d2fde5352431f6612e037dda3f0124b1500c
Chris Manson <chris@manson.ie>
2015-02-13 15:29:51 +0000

    trying to make it work using treeForStyles

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 /* jshint node: true */
+
+var compileSass = require('broccoli-sass');
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-toggle'
+  name: 'ember-cli-toggle',
+
+
+  treeForStyles: function () {
+    this._super.treeForStyles();
+    return compileSass([__dirname + '/addon/styles/'], 'default.scss', '/assets/toggle-theme.css');
+  }
 };
