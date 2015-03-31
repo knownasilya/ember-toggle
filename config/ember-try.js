edadd8947e1491724b9820835f89357b075d20fa
Ilya Radchenko <ilya@burstcreations.com>
2015-03-31 16:51:33 -0400

    Add ember-try, fix x-toggle test

---
+++ config/ember-try.js
@@ -0,0 +1,22 @@
+module.exports = {
+  scenarios: [
+    {
+      name: 'Ember 1.11',
+      dependencies: {
+        'ember': '1.11.0'
+      }
+    },
+    {
+      name: 'Ember beta',
+      dependencies: {
+        'ember': 'beta'
+      }
+    },
+    {
+      name: 'Ember canary',
+      dependencies: {
+        'ember': 'canary'
+      }
+    }
+  ]
+};
--- tests/unit/components/x-toggle-test.js
+++ tests/unit/components/x-toggle-test.js
@@ -3,19 +3,19 @@ import {
   test
 } from 'ember-qunit';
 
-moduleForComponent('x-toggle', 'XToggleComponent', {
+moduleForComponent('x-toggle', {
   // specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
 });
 
-test('it renders', function() {
-  expect(2);
+test('it renders', function(assert) {
+  assert.expect(2);
 
   // creates the component instance
   var component = this.subject();
-  equal(component._state, 'preRender');
+  assert.equal(component._state, 'preRender');
 
-  // appends the component to the page
-  this.append();
-  equal(component._state, 'inDOM');
+  // renders the component to the page
+  this.render();
+  assert.equal(component._state, 'inDOM');
 });
