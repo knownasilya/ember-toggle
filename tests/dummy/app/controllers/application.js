35e12b53c66780b5885414632426f2813e954daa
Ilya Radchenko <ilya@burstcreations.com>
2015-06-11 08:47:02 -0400

    Tweak css, update some demo code

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  toggled: true,
+  toggled2: false,
 	boundToggle: false,
-	bT2: false,
+
 	actions: {
+    overloaded(toggled, state) {
+      this.set('state', state);
+    },
+
+    overloaded2(toggled, state) {
+      this.set('state2', state);
+    },
+
 		checkboxToggled: function () {
 			this.toggleProperty('toggled');
 		}
