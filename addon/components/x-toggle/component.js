151b8dea897ec721acb96492333b8a9b9b277571
Ken Snyder <ken@ken.net>
2015-06-16 13:25:13 +0100

    boolean and non-string values
    I coded this all directly into github so it needs a test or two but it's close.

--- addon/components/x-toggle/component.js
+++ addon/components/x-toggle/component.js
@@ -5,8 +5,16 @@ const {
   on,
   run,
   computed,
-  observer
+  observer,
+  typeOf
 } = Ember;
+const getValue = item => {
+  return typeOf(item) === 'string' ? item.split(':')[0] : item;
+};
+const getLabel = item => {
+  return typeOf(item) === 'string' ? item.split(':')[1] : item;
+};
+
 
 export default Ember.Component.extend({
   layout: layout,
@@ -18,17 +26,16 @@ export default Ember.Component.extend({
     'isToggled:x-toggle-container-checked',
     'disabled:x-toggle-container-disabled'
   ],
+  // default value states
+  on: true,
+  off: false,
 
   onLabel: computed('on', function () {
-    return this.get('on').indexOf(':') > -1 ?
-      this.get('on').substr(0,this.get('on').indexOf(':')) :
-      this.get('on');
+   return getLabel(this.get('on'));
   }),
 
   offLabel: computed('off', function () {
-    return this.get('off').indexOf(':') > -1 ?
-      this.get('off').substr(0,this.get('off').indexOf(':')) :
-      this.get('off');
+   return getLabel(this.get('off'));
   }),
 
   themeClass: computed('theme', function () {
@@ -44,10 +51,8 @@ export default Ember.Component.extend({
   toggledChanged: observer('isToggled', function () {
 	  var debounce = run.debounce(this, function () {
       var toggled = this.get('isToggled');
-      var offIndex = this.get('off').indexOf(':');
-      var onIndex = this.get('on').indexOf(':');
-      var offState = offIndex > -1 ? this.get('off').substr(offIndex + 1) : false;
-      var onState = onIndex > -1 ? this.get('on').substr(onIndex + 1) : true;
+      var offState = getValue(this.get('off'));
+      var onState = getValue(this.get('on'));
       var state = toggled ? onState : offState;
 
       this.sendAction('on-toggle', toggled, state);
