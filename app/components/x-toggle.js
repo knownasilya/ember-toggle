d80065fb8ddfb674c44bc1f9a5348eb4cb154f74
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2015-07-18 15:04:37 +0100

    Fix toggle debounce cleanup

--- app/components/x-toggle.js
+++ app/components/x-toggle.js
@@ -58,23 +58,37 @@ export default Ember.Component.extend({
   })),
 
   valueObserver: on('init', observer('value', function() {
-	  Ember.run.debounce(this, function () {
-      var value = this.get('value');
-      var offIndex = this.get('off').indexOf(':');
-      var onIndex = this.get('on').indexOf(':');
-      var offState = offIndex > -1 ? this.get('off').substr(offIndex + 1) : false;
-      var onState = onIndex > -1 ? this.get('on').substr(onIndex + 1) : true;
+    var debounce = this.get('debounce');
+    if (!debounce) {
+      debounce = run.debounce(this, function () {
+        var value = this.get('value');
+        var offIndex = this.get('off').indexOf(':');
+        var onIndex = this.get('on').indexOf(':');
+        var offState = offIndex > -1 ? this.get('off').substr(offIndex + 1) : false;
+        var onState = onIndex > -1 ? this.get('on').substr(onIndex + 1) : true;
 
-      if (value === onState) {
-        this.set('toggled', true);
-      } else {
-        this.set('toggled', false);
-        this.set('value', offState);
-      }
-	  }, 500);
+        if (value === onState) {
+          this.set('toggled', true);
+        } else {
+          this.set('toggled', false);
+          this.set('value', offState);
+        }
+      }, 500);
+
+      this.set('debounce', debounce);
+    }
   })),
 
-  click: function (event) {
+  clearDebounce: on('willDestroyElement', function () {
+    var debounce = this.get('debounce');
+
+    if (debounce) {
+      run.cancel(debounce);
+      this.set('debounce', null);
+    }
+  }),
+
+  click(event) {
     event.stopPropagation();
   }
 });
