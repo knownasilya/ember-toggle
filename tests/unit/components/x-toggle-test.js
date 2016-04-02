2c827ce24f93cf59cc4b8e25d21ca2cc60ecc611
Jacob Jewell <jacobjewell@eflexsystems.com>
2016-04-02 15:03:53 -0400

    add more tests

--- tests/unit/components/x-toggle-test.js
+++ tests/unit/components/x-toggle-test.js
@@ -5,8 +5,7 @@ import {
 import Ember from 'ember';
 
 moduleForComponent('x-toggle', {
-  // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  unit: true
 });
 
 test('it renders', function(assert) {
@@ -21,22 +20,73 @@ test('it renders', function(assert) {
   assert.equal(component._state, 'inDOM');
 });
 
-test('changing toggled changes state', function (assert) {
+test('changing toggled changes state', function(assert) {
   assert.expect(3);
 
   var component = this.subject();
 
   assert.equal(this.$('input.x-toggle').prop('checked'), false, 'unchecked by default');
 
-  Ember.run(function () {
+  Ember.run(() => {
     component.set('toggled', true);
   });
 
   assert.equal(this.$('input.x-toggle').prop('checked'), true, 'checked when toggled: true');
 
-  Ember.run(function () {
+  Ember.run(() => {
     component.set('toggled', false);
   });
 
   assert.equal(this.$('input.x-toggle').prop('checked'), false, 'unchecked when toggled: false');
 });
+
+test('changing disabled', function(assert) {
+  assert.expect(3);
+
+  var component = this.subject();
+
+  assert.equal(this.$('input.x-toggle').prop('disabled'), false, 'disabled by default');
+
+  Ember.run(() => {
+    component.set('disabled', true);
+  });
+
+  assert.equal(this.$('input.x-toggle').prop('disabled'), true, 'disabled when disabled: true');
+
+  Ember.run(() => {
+    component.set('disabled', false);
+  });
+
+  assert.equal(this.$('input.x-toggle').prop('disabled'), false, 'disabled when disabled: false');
+});
+
+test('toggle action fires when toggled changed from false to true', function (assert) {
+  assert.expect(1);
+
+  var component = this.subject({ toggled: false });
+
+  Ember.run(() => {
+    component.set('toggle', (toggleVal) => {
+      assert.ok(toggleVal);
+      component.set('toggle', null);
+    });
+  });
+
+  this.$('input.x-toggle').click();
+});
+
+test('toggle action fires when toggled changed from true to false', function (assert) {
+  assert.expect(1);
+
+  var component = this.subject({ toggled: true });
+
+  Ember.run(() => {
+    component.set('toggle', (toggleVal) => {
+      console.log(toggleVal);
+      assert.notOk(toggleVal);
+    });
+  });
+
+  this.$('input.x-toggle').click();
+});
+
