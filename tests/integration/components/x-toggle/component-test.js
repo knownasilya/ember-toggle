ae1a0b689f7e965793f282a8223c139422d0a24c
Ilya Radchenko <ilya@burstcreations.com>
2017-04-05 23:01:07 -0400

    feat: simplify all the things
    BREAKING CHANGE: Removed label values and made the action simple, passing only a single boolean value.
    
    Fixes the value change bug. More changes to come.

--- tests/integration/components/x-toggle/component-test.js
+++ tests/integration/components/x-toggle/component-test.js
@@ -14,20 +14,23 @@ test('it renders', function(assert) {
 
 test('changing disabled property disables component', function(assert) {
   this.set('disabled', true);
+  this.set('value', false);
   this.render(hbs`{{x-toggle
-    value='off'
+    value=value
     disabled=disabled
   }}`);
   assert.equal($('input.x-toggle').prop('disabled'), true);
+  assert.equal(this.get('value'), false);
+  this.$('div.x-toggle-btn').click();
+  assert.equal($('input.x-toggle').prop('disabled'), true);
+  assert.equal(this.get('value'), false);
 });
 
 test('clicking component triggers onToggle action', function(assert) {
   const done = assert.async();
-  this.set('myValue', 'off');
-  this.on('onToggle', hash => {
-    assert.equal(hash.code, 'toggled', 'onToggle code is toggle');
-    assert.equal(hash.oldValue, 'off', 'proper old value');
-    assert.equal(hash.newValue, 'on', 'proper new value');
+  this.set('myValue', false);
+  this.on('onToggle', val => {
+    assert.equal(val, true, 'new value set');
 
     this.set('completed', true);
     done();
@@ -44,52 +47,30 @@ test('clicking component triggers onToggle action', function(assert) {
       done();
     }
   }, 1000);
-
 });
 
 if (emberVersionGTE(2,0)) {
-  test('clicking component works with default on/off and mut helper', function(assert) {
-    this.set('value', 'off');
-    this.render(hbs`{{x-toggle
-      value=value
-      onToggle=(mut value)
-    }}`);
-    assert.equal(this.get('value'), 'off');
-    this.$('div.x-toggle-btn').click();
-    assert.equal(this.get('value'), 'on');
-  });
-
-  test('component toggles when container changes value', function(assert) {
-    this.set('value', 'off');
-    this.render(hbs`{{x-toggle
-      value=value
-      onToggle=(mut value)
-    }}`);
-    assert.equal(this.get('value'), 'off');
-    this.set('value', 'on');
-    assert.equal(this.get('value'), 'on');
-  });
-
   test('clicking component works with bespoke values and mut helper', function(assert) {
-    this.set('value', 'foo');
+    this.set('value', false);
     this.render(hbs`{{x-toggle
       value=value
-      offLabel='Foo::foo'
-      onLabel='Bar::bar'
-      onToggle=(mut value)
+      offLabel='Foo'
+      onLabel='Bar'
+      showLabels=true
+      onToggle=(action (mut value))
     }}`);
-    assert.equal(this.get('value'), 'foo');
-    assert.equal(this.$('div.x-toggle-btn').data('tg-off'), 'Foo', '"off" property set on toggle');
-    assert.equal(this.$('div.x-toggle-btn').data('tg-on'), 'Bar', '"on" property set on toggle');
+    assert.equal(this.get('value'), false);
+    assert.equal(this.$('.off-label').text().trim(), 'Foo', '"off" property set on toggle');
+    assert.equal(this.$('.on-label').text().trim(), 'Bar', '"on" property set on toggle');
     this.$('div.x-toggle-btn').click();
-    assert.equal(this.get('value'), 'bar', 'click toggles value');
+    assert.equal(this.get('value'), true, 'click toggles value');
   });
 
   test('clicking component works with boolean true/false', function(assert) {
     this.set('value', false);
     this.render(hbs`{{x-toggle
       value=value
-      onToggle=(mut value)
+      onToggle=(action (mut value))
       onLabel=true
       offLabel=false
     }}`);
@@ -102,12 +83,27 @@ if (emberVersionGTE(2,0)) {
     this.set('value', false);
     this.render(hbs`{{x-toggle
       value=value
-      onToggle=(mut value)
-      onLabel='Yes::true'
-      offLabel='No::false'
+      onToggle=(action (mut value))
+      onLabel='Yes'
+      offLabel='No'
     }}`);
     assert.equal(this.get('value'), false);
     this.$('div.x-toggle-btn').click();
     assert.equal(this.get('value'), true);
+  });
+
+  test('value can be set by changing the value property', function(assert) {
+    this.set('value', false);
+    this.set('show', false);
+
+    this.render(hbs`
+      {{x-toggle value=value onToggle=(action (mut value))}}
+    `);
+
+    this.set('value', true);
+    assert.equal(this.$('.x-toggle-container').hasClass('x-toggle-container-checked'), true);
+
+    this.set('value', false);
+    assert.equal(this.$('.x-toggle-container').hasClass('x-toggle-container-checked'), false);
   });
 }
