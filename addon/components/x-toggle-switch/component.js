93c7f47335c407202dbef613da063156e2fb8157
Ilya Radchenko <ilya@burstcreations.com>
2017-05-18 11:12:30 -0400

    fix: remove injection of browser checker

--- addon/components/x-toggle-switch/component.js
+++ addon/components/x-toggle-switch/component.js
@@ -1,6 +1,5 @@
 import Component from 'ember-component';
 import computed from 'ember-computed';
-import service from 'ember-service/inject';
 import layout from './template';
 
 export default Component.extend({
@@ -8,8 +7,6 @@ export default Component.extend({
   tagName: 'span',
   classNames: ['x-toggle-container'],
   classNameBindings: ['size', 'disabled:x-toggle-container-disabled', 'value:x-toggle-container-checked'],
-
-  browserChecker: service(),
 
   themeClass: computed('theme', function() {
     return `x-toggle-${this.get('theme') || 'default'}`;
