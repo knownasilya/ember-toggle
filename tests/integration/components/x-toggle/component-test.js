dce89b2cad7bc385a982a56268973ec35476692f
Nana Adjei Manu <claeusdev@gmail.com>
2021-12-08 03:45:32 +0000

    chore: update tests  hbs import

--- tests/integration/components/x-toggle/component-test.js
+++ tests/integration/components/x-toggle/component-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, click } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 import emberVersionGTE from 'ember-test-helpers/has-ember-version';
 import { pan } from 'ember-gesture-modifiers/test-support';
 
