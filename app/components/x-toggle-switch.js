014374f63d0b286ec448502516990fe59dc20a54
Ilya Radchenko <knownasilya@gmail.com>
2022-03-18 20:41:33 -0400

    wip: v2 addon codemod
    TODO: figure out what to do with index.js
    
    Next steps:
     - customize the rollup.config.js to suit your needs
     - audit dependencies of both the new addon and test-app
     - run `yarn install`
     - run `yarn run lint:fix` in each workspace
     - test your addon `cd test-app; yarn test`

--- app/components/x-toggle-switch.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-toggle/components/x-toggle-switch';
--- tests/dummy/app/helpers/not.js
+++
@@ -1,7 +0,0 @@
-import { helper as buildHelper } from '@ember/component/helper';
-
-export function not([val] /*, hash*/) {
-  return !val;
-}
-
-export default buildHelper(not);
